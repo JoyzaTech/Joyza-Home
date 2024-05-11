db22bd2ee6f37343489a4d43947f532e156db86a
conner12345678 <cehly065@west-mec.org>
2024-05-10 21:20:43 -0700

    slide bar functionality

--- js/index.js
+++ js/index.js
@@ -5,22 +5,43 @@ $(document).ready(function(){
 
     let dimensions = [width, height]
 
-    let slide = 1;
+    let slide = 3;
 
     $('#next').click(function(){
-        if($('#item3').css('grid-area', 'slide3')){
-            $('#item1').css('visibility', 'hidden')
-            $('#item4').css('visibility', 'visible')
-            for(let i = 2; i<=4;i++){
-                $('#item' + i).css('grid-area', 'slide' + (i-1))
-            }
+        if(slide < 4){
+            $('#item'+(slide-2)).css('visibility', 'hidden')
+            $('#item'+(slide-2)).css('grid-area', 'none')
+            $('#item'+(slide-1)).css('grid-area', 'slide1')
+            $('#item'+(slide)).css('grid-area', 'slide2')
+            $('#item'+(slide+1)).css('grid-area', 'slide3')
+            $('#item'+(slide+1)).css('visibility', 'visible')
+            $('#item'+slide).css('visibility', 'visible')
+            $('#item'+(slide-1)).css('visiibility', 'visible')
             slide++
         }
-        else{
-            $('#item' + slide).css('visibility', 'hidden')
-            $('#item' + (slide-1)).css('visibility', 'visible')
-            $('#item' + (slide+1)).css('grid-area', 'slide1')
+        else if(slide == 4){
+            $('#item'+(slide-2)).css('visibility', 'hidden')
+            $('#item'+(slide-2)).css('grid-area', 'none')
+            $('#item'+(slide-1)).css('grid-area', 'slide1')
+            $('#item'+(slide)).css('grid-area', 'slide2')
+            $('#item'+(slide+1)).css('grid-area', 'slide3')
+            $('#item'+(slide+1)).css('visibility', 'visible')
+            $('#item'+slide).css('visibility', 'visible')
+            $('#item'+(slide-1)).css('visiibility', 'visible')
         }
     });
     
+    $('#back').click(function(){
+        if(slide >= 3){
+            $('#item' + slide).css('visibility', 'hidden')
+            $('#item' + (slide-1)).css('visibility', 'visible')
+            $('#item' + (slide-2)).css('visibility', 'visible')
+            $('#item' + (slide-3)).css('visibility', 'visible')
+            $('#item' + slide).css('grid-area', 'none')
+            $('#item' + (slide-1)).css('grid-area', 'slide3')
+            $('#item' + (slide-2)).css('grid-area', 'slide2')
+            $('#item' + (slide-3)).css('grid-area', 'slide1')
+            slide--
+        }
+    })
 });
