d575fea2d34bb8ccc93b1927d276461e9434c7ee
conner12345678 <cehly065@west-mec.org>
2024-05-10 17:38:44 -0700

    working on slider yipee

--- js/index.js
+++ js/index.js
@@ -1,102 +1,20 @@
 $(document).ready(function(){
 
-let width = window.innerWidth
-let height = window.innerHeight
-
-let dimensions = [width, height]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    let width = window.innerWidth
+    let height = window.innerHeight
+
+    let dimensions = [width, height]
+
+    let slide = 1
+    $('#next').click(function(){
+        if(slide == 1){
+            $('#item' + slide).css('visibility', 'hidden')
+            $('#item' + (slide+3)).css('visibility', 'visible')
+            slide++
+            for(;slide<=4;slide++){
+                $('#item' + slide).css('grid-area', 'slide' + (slide-1))
+            }
+            slide = 2
+        }
+    })
 });
